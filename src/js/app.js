d886038a29372a7c1a237774b4fe54ff5df630b3
Luís Melo <luis.f.duraes.melo@gmail.com>
2022-04-11 08:31:16 +0100

    CSS smooth scroll applied

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,3 @@
 "use strict";
 console.clear();
 console.log("testing...");
-const btnsNavscrollto = document.querySelectorAll(".btn_scroll");
-const sections = document.querySelectorAll(".section");
-const section_1 = document.querySelector("#section_1");
-const btnscrollto = document.querySelector("#btn_scroll");
-const navScrollTo = btnsNavscrollto.forEach((btn) => {
-    btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function (e) {
-        e.preventDefault();
-        sections.forEach((section) => section.scrollIntoView({ behavior: "smooth" }));
-    });
-});
-btnscrollto === null || btnscrollto === void 0 ? void 0 : btnscrollto.addEventListener("click", function (e) {
-    e.preventDefault();
-    section_1 === null || section_1 === void 0 ? void 0 : section_1.scrollIntoView({
-        behavior: "smooth",
-    });
-});
