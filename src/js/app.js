ce91a15888e1ba528da4ca840d63b681e3896426
Luís Melo <luis.f.duraes.melo@gmail.com>
2022-04-11 21:33:39 +0100

    Login form implemented

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,23 @@
 "use strict";
 console.clear();
 console.log("testing...");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnLogin = document.querySelector(".btn_login");
+const btnCloseModal = document.querySelector(".btn_close_modal");
+const openModal = (e) => {
+    e.preventDefault();
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+const closeModal = () => {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+btnLogin.addEventListener("click", openModal);
+btnCloseModal.addEventListener("click", closeModal);
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+});
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -3,9 +3,33 @@ console.log("testing...");
 
 ////////////////////////////
 //DOM ELEMENTS
-
+const modal = document.querySelector(".modal") as HTMLElement;
+const overlay = document.querySelector(".overlay") as HTMLElement;
+const btnLogin = document.querySelector(".btn_login") as HTMLButtonElement;
+const btnCloseModal = document.querySelector(
+  ".btn_close_modal"
+) as HTMLButtonElement;
 ////////////////////////////
 //LOGIN MODAL
+
+const openModal = (e: { preventDefault: () => void }) => {
+  e.preventDefault();
+
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+btnLogin.addEventListener("click", openModal);
+btnCloseModal.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
 
 ////////////////////////////
 //REGISTER MODAL
